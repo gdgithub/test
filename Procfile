ea4f97d170851d16faacf008e9f646d6876b9f6d
Starlin Gil <starlin.gil.cruz@gmail.com>
2016-07-08 10:31:35 -0400

    update project

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn main.wsgi -b 0.0.0.0:$PORT
--- main/settings/contest.py
+++ main/settings/contest.py
@@ -1,15 +1,19 @@
 # coding=utf-8
-from .base import *
+from base import *
 
 SITE_ID = 1
 
 DEBUG = False
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'd68h5nkrporfnk',
+            'USER': 'ulddsnxfpfcblu',
+            'PASSWORD': 'VyuNwr5ZDe6Z5ZN8dS7g6BGlWj',
+            'HOST': 'ec2-54-235-208-104.compute-1.amazonaws.com',
+            'PORT': '5432',
+        }
     }
-}
 
 
