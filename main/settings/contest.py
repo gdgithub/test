9a767a6d9fa40a9b6abd623637edaebbdf2f3613
Starlin Gil <starlin.gil.cruz@gmail.com>
2016-07-09 22:19:01 -0400

    fixed bug

--- main/settings/contest.py
+++ main/settings/contest.py
@@ -7,14 +7,14 @@ ALLOWED_HOSTS = ['*']
 DEBUG = False
 
 DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'd68h5nkrporfnk',
-            'USER': 'ulddsnxfpfcblu',
-            'PASSWORD': 'VyuNwr5ZDe6Z5ZN8dS7g6BGlWj',
-            'HOST': 'ec2-54-235-208-104.compute-1.amazonaws.com',
-            'PORT': '5432',
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd68h5nkrporfnk',
+        'USER': 'ulddsnxfpfcblu',
+        'PASSWORD': 'VyuNwr5ZDe6Z5ZN8dS7g6BGlWj',
+        'HOST': 'ec2-54-235-208-104.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
+}
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
