f644f19dbf0d0f7a654d353c0598d50c9235a0f1
Ivan <ivangilcruz@gmail.com>
2016-07-25 11:15:42 -0400

    project modified

--- manage.py
+++ manage.py
@@ -1,1 +1,12 @@
-MyOrder/src/manage.py
+#!/usr/bin/env python
+import os
+import sys
+import json
+
+if __name__ == "__main__":
+	#env = json.loads(open('conf.json', 'r').readlines()[0])['env']
+	#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings.{0}".format(env))
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "src.main.settings.contest")
+
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
--- src/main/settings/base.py
+++ src/main/settings/base.py
@@ -15,8 +15,8 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+BASE_DIR = os.path.dirname(PROJECT_ROOT)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
@@ -43,10 +43,10 @@ PREREQ_APPS = (
 )
 
 PROJECT_APPS = (
-    'uadmin',
-    'home',
-    'directories',
-    'management'
+    'src.uadmin',
+    'src.home',
+    'src.directories',
+    'src.management'
 )
 
 INSTALLED_APPS = PREREQ_APPS + PROJECT_APPS
